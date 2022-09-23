a110bd97a99a7e4ee913fbd5639b0ceb57f97f97
Eve <evedieimmer@gmail.com>
2022-09-23 10:46:53 +0700

    change signUp activity

--- app/src/main/java/ru/gorbunova/tictactoe/presentation/auth/signUp/FragmentSignUp.kt
+++ app/src/main/java/ru/gorbunova/tictactoe/presentation/auth/signUp/FragmentSignUp.kt
@@ -1,6 +1,12 @@
 package ru.gorbunova.tictactoe.presentation.auth.signUp
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_sign_up.*
@@ -8,6 +14,7 @@ import kotlinx.android.synthetic.main.fragment_sign_up.btnSignUp
 import moxy.presenter.InjectPresenter
 import moxy.presenter.ProvidePresenter
 import ru.gorbunova.tictactoe.R
+import ru.gorbunova.tictactoe.databinding.FragmentSignUpBinding
 import ru.gorbunova.tictactoe.domain.di.component.DaggerAppComponent
 import ru.gorbunova.tictactoe.presentation.auth.INavigateRouter
 import soft.eac.appmvptemplate.views.ABaseFragment
@@ -26,7 +33,9 @@ class FragmentSignUp : ABaseFragment(R.layout.fragment_sign_up), ISignUpView {
         DaggerAppComponent.create().inject(this)
     }
 
-//     fun getViewId() = R.layout.fragment_sign_up
+    private val binding: FragmentSignUpBinding get() = getViewBinding()
+    private val GALLERY_REQUEST = 1
+    private val REQUEST_TAKE_PHOTO = 1
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -45,6 +54,55 @@ class FragmentSignUp : ABaseFragment(R.layout.fragment_sign_up), ISignUpView {
                 return@setOnClickListener
             }
             presenter.registration(login, password)
+        }
+
+        ivAvatar.setOnClickListener{
+            activity?.let {
+                AlertDialog.Builder(it)
+                    .setTitle("Загрузить фото")
+                    .setMessage("Выберите:")
+                    .setPositiveButton("Открыть галерею") { dialog, _ ->
+                        dialog.dismiss()
+
+                        val intent = Intent(Intent.ACTION_PICK)
+                        intent.type = "image/*"
+                        startActivityForResult(intent,GALLERY_REQUEST)
+                    }
+                    .setNegativeButton("Открыть камеру") { dialog, _ ->
+                        dialog.dismiss()
+
+                        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        try {
+                            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                        } catch (e: ActivityNotFoundException) {
+                            e.printStackTrace()
+                        }
+                    }
+
+            }
+
+        }
+
+//        Glide.with(this)
+//            .load(avatar)
+//            .centerCrop()
+//            .into(binding.ivAvatar)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GALLERY_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            ivAvatar.setImageURI((data.data))
+        }
+        else {
+            toast("Something went wrong")
+        }
+
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK && data != null) {
+                ivAvatar.setImageBitmap(data.extras?.get("data") as Bitmap)
+        } else {
+            toast("Something went wrong")
         }
     }
 
